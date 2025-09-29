d008ffe90b716688436fd171e8ce07b038176d35
somagouda <madagoudasomagouda@gmail.com>
2025-09-29 21:45:24 +0530

    create signup api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose  from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";    
+import authRouter from "./routes/auth.route.js";    
 dotenv.config();
 mongoose.connect(process.env.MONGO).then(()=>{
     console.log("connected to mongoDB");
@@ -9,8 +10,10 @@ mongoose.connect(process.env.MONGO).then(()=>{
     console.log(err);
 });
 const app=express();
+app.use(express.json());
 app.listen(4000,()=>{
     console.log("server is running on port 4000!!!!");
 });
-app.use("/api/user",userRouter);   
+app.use("/api/user",userRouter);  
+app.use("/api/auth",authRouter);
 
