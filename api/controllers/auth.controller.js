914f5d4bc658c8239c8ef161ce87e0429246c375
somagouda <madagoudasomagouda@gmail.com>
2025-10-01 18:54:41 +0530

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
    const { username, email, password } = req.body;
@@ -11,5 +12,25 @@ export const signup = async (req, res, next) => {
    res.status(201).json({ message: "User registered successfully" });
    } catch (error){
     next(error);
+   }
+};
+export const signin = async (req, res, next) => {
+   const { email, password } = req.body;
+   try {
+      const user = await User.findOne({ email });
+      if (!user) {
+         return res.status(404).json({ message: "User not found" });
+      }
+      const isMatch = bcrypt.compareSync(password, user.password);
+      if (!isMatch) {
+         return res.status(401).json({ message: "Invalid credentials!" });
+      }
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+      const{password:pwd,...otherDetails}=user._doc;
+       res.cookie("access_token", token, {
+         httpOnly: true
+      }).status(200).json({ message: "User signed in successfully!", user: otherDetails });
+   } catch (error) {
+      next(error);
    }
 };
